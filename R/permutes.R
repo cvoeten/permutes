56569c39522d97a6fddb2776cb1903a43bf9463e
Cesko Voeten <c.c.voeten@hum.leidenuniv.nl>
2017-08-19 10:25:56 +0200

    Always calculate both p-values and w2

--- R/permutes.R
+++ R/permutes.R
@@ -2,16 +2,14 @@
 #' @param formula A formula of the following form: `outcome ~ predictors | timepoint variables'. Multivariate outcomes (e.g. 32 EEG electrodes) are supported; use `cbind(Fp1,Fp2,etc) ~ predictors | timepoint'.
 #' @param data The dataset referencing these predictors.
 #' @param subset If specified, will only analyze the specified subset of the data.
-#' @param w2 If set to TRUE, will calculate effect sizes (omega squared) rather than p-values. Note that the value will be clamped to zero if the calculated omega-squared statistic would be negative.
-#' @param w2.onlysig If w2 is TRUE, will discard (i.e. set to NA) effect sizes of non-significant parameters.
 #' @param parallel Whether to parallelize the permutation testing using plyr's `parallel' option. Needs some additional set-up; see the plyr documentation.
 #' @return A dataframe of p-values.
 #' @import plyr lmPerm
 #' @export
-permu.test <- function (formula,data,subset=NULL,w2=FALSE,w2.onlysig=FALSE,parallel=FALSE) {
+permu.test <- function (formula,data,subset=NULL,parallel=FALSE) {
 	errfun <- function (e) {
 		warning(e)
-		return(data.frame(timepoint=NA,factor=NA,p=NA))
+		return(data.frame(timepoint=NA,factor=NA,p=NA,w2=NA))
 	}
 	if (formula[[1]] != '~') stop("Invalid formula (first operator is not '~')")
 	indep <- formula[[3]]
@@ -29,26 +27,22 @@ permu.test <- function (formula,data,subset=NULL,w2=FALSE,w2.onlysig=FALSE,paral
 			if (ncol(res) != 5) return(errfun(paste0('Timepoint ',t,' did not have more observations than predictors; the ANOVA is unidentifiable')))
 			factors <- rownames(res)
 			pvals <- res[[5]]
-			if (w2) {
-				df <- res[[1]]
-				SS <- res[[2]]
-				nr <- length(df)
-				dff <- df[-nr]
-				dfe <- df[ nr]
-				SSf <- SS[-nr]
-				SSe <- SS[ nr]
-				MSe <- SSe/dfe
-				w2 <- (SSf - dff * MSe) / (sum(SS) + MSe)
-				w2 <- pmax(w2,0)
-				w2 <- c(w2,NA) #the residuals
-				if (w2.onlysig) w2[pvals >= .05] <- NA
-				pvals <- w2
-			}
-			data.frame(timepoint=t,factor=factors,p=pvals,stringsAsFactors=F)
+			df <- res[[1]]
+			SS <- res[[2]]
+			nr <- length(df)
+			dff <- df[-nr]
+			dfe <- df[ nr]
+			SSf <- SS[-nr]
+			SSe <- SS[ nr]
+			MSe <- SSe/dfe
+			w2 <- (SSf - dff * MSe) / (sum(SS) + MSe)
+			w2 <- pmax(w2,0)
+			w2 <- c(w2,NA) #the residuals
+			data.frame(timepoint=t,factor=factors,p=pvals,w2=w2,stringsAsFactors=F)
 		},.parallel=F,.id='measure')
 	},.parallel=parallel,.id=NULL)
 	if (ncol(ret) < 4) ret <- cbind(as.character(formula[[2]]),ret,stringsAsFactors=F) #ldply will not have generated the first column if the outcome was univariate
-	colnames(ret) <- c('measure',timepoint.var,'factor',ifelse(w2,'w2','p'))
+	colnames(ret)[2] <- timepoint.var
 	ret$measure <- sub('^ Response ','',ret$measure)
 	ret$factor <- sub(' +$','',ret$factor)
 	ret <- ret[ret$factor != 'Residuals',]
@@ -58,11 +52,12 @@ permu.test <- function (formula,data,subset=NULL,w2=FALSE,w2.onlysig=FALSE,paral
 
 #' Create a heatmap of the results of permutation testing.
 #' @param data Output of permu.test. You may want to subset it if you want to simulate zooming in.
+#' @param plot Either 'p', to create a heatmap of the p-values, or 'w2' to use the effect size measure (omega squared) instead.
 #' @param breaks The granularity of the labels of the x axis. Pass `unique(data[,2])' to get a tick for every timepoint. Combine this trick with subsetting of your dataset, and perhaps averaging over all your dependent variables, to `zoom in' on your data to help you determine precisely where significance begins and stops to occur.
 #' @return A ggplot2 object containing a heatmap of p-values.
 #' @import ggplot2 viridis
 #' @export
-plot.permutes <- function (x,y=NULL,breaks=NULL) {
+plot.permutes <- function (x,y=NULL,plot=c('p','w2'),breaks=NULL) {
 	if (!is.null(y)) {
 		# User provided vectors instead of a dataframe
 		if ('data.frame' %in% class(x)) stop("Unable to understand 'y' argument if 'x' is not a vector")
@@ -73,8 +68,8 @@ plot.permutes <- function (x,y=NULL,breaks=NULL) {
 		data <- x
 		p <- ggplot(data=data,aes_string(x=colnames(data)[2],y=colnames(data)[1]))
 	}
-	val.name <- colnames(data)[4]
-	p <- p + geom_tile(aes_string(fill=val.name)) + scale_fill_viridis(option='plasma',direction=ifelse(val.name == 'p',-1,1))
+	plot <- plot[1]
+	p <- p + geom_tile(aes_string(fill=plot)) + scale_fill_viridis(option='plasma',direction=ifelse(plot == 'p',-1,1))
 	p <- p + if (is.null(breaks)) scale_x_continuous(expand=c(0,0)) else scale_x_continuous(expand=c(0,0),breaks=breaks)
 	p <- p + if (length(unique(data$factor)) == 1) scale_y_discrete(expand=c(0,0)) else facet_wrap(~factor,ncol=1)
 	p <- p + xlab(colnames(data)[2]) + ylab(colnames(data)[1])
